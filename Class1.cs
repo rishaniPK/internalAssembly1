cce003567241cab50bee150d4176ac8e5807ce2b
Admin <Admin@DESKTOP-89NTJEN>
2019-06-24 13:14:05 +1200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace internalAssembly1
+{
+   public class Class1
+    {
+   protected internal int x = 30;
+   protected internal int y = 10;
+
+
+        public int add_numb()
+        {
+            return x + y;
+        }
+    }
+}
---
+++ childassembly.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace internalAssembly1
+{
+   public class childassembly
+    {
+        Class1 i = new Class1();
+        int a = 1;
+        int b = 3;
+        public int add()
+        {
+            return i.x + i.y + a + b;
+        }
+
+    }
+}
